b009d4460a5fb99eb5934e167dc2696ea4d934d7
baronvonpurr <robinhood.svensson@hotmail.com>
2023-09-13 11:12:10 +0200

    Add project files.

---
+++ Uppgift2.4/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Uppgift2_4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Skriv in ett tal");
+            double tal1 = double.Parse(Console.ReadLine());
+            Console.WriteLine("Skriv ett tal till");
+            double tal2 = double.Parse(Console.ReadLine());
+            Console.WriteLine("Adderat blir det " + (tal1 + tal2));
+            Console.WriteLine("Multiplicerat blir det " + (tal1 * tal2));
+            Console.ReadKey();
+        }
+    }
+}
